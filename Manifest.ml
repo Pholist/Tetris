5a8865cbf62b9b55501280cf4e60b49a5f5f26ea
salma al jaouhari <salma.al.jaouhari@gmail.com>
2021-10-05 09:00:08 +0200

    CreateProject

---
+++ Manifest.ml
@@ -0,0 +1,8 @@
+Manifest-Version: 1.0
+Ant-Version: Apache Ant 1.10.5
+Created-By: 13.0.2+8 (Oracle Corporation)
+main-class: fr.ubo.tetris.Tetris
+implementation-version: 1
+build-by: user
+Class-Path: Tetris.jar
+
